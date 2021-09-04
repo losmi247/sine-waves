d67c20b6107216df4c0961ad94a1cfee3044e56f
losmi247 <40543607+losmi247@users.noreply.github.com>
2021-09-04 20:06:35 +0200

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -17,11 +17,4 @@ function drawsomething(something) {
   ctx.moveTo(0, 0);
   ctx.lineTo(200, 100);
   ctx.stroke();
-  
-  <!-- ctx.beginPath();
-  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-  ctx.stroke();
-  
-  ctx.font = "30px Arial";
-  ctx.fillText("Hello World", 10, 50); -->
 }
